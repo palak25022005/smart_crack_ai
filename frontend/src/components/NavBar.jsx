4da71a0714c9fc446a81429ce6d954c3cae68a1a
pranjal-8757 <pranjalghaste@gmail.com>
2025-03-28 23:50:59 +0530

    Navbar change

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,9 +5,7 @@ const NavBar = () => {
   return (
     <nav className="bg-blue-600 text-white p-4 flex justify-between items-center shadow-lg">
       {/* Website Name */}
-      <h1 className="text-2xl font-bold text-black" style={{ WebkitTextStroke: "1px black" }}>
-  SmartCrack AI
-</h1>
+ 
 
       {/* Navigation Links */}
          
