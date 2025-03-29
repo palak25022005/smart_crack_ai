1f7822b68a85a7a093f7aaae463667b30dff66b1
Palak K <palakkongale@gmail.com>
2025-03-29 14:50:49 +0530

    fetch bug solved

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv"
 import cors from "cors"
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.js";
-import syllabusRoutes from "./routes/syllabusRoutes.js";
 
 import { connectDB } from "./config/db.js";
 dotenv.config();
@@ -15,7 +14,7 @@ app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174','http:/
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/syllabus",syllabusRoutes)
+
 
 const PORT = process.env.PORT || 5001;
  app.listen(PORT,()=>{
